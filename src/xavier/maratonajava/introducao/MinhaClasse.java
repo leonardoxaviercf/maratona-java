c71c2aa7315bd05fd2e6e10b7fc76078f3639c97
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-06 20:11:41 -0300

    Comentários e suas diferenças

--- src/xavier/maratonajava/introducao/MinhaClasse.java
+++ src/xavier/maratonajava/introducao/MinhaClasse.java
@@ -1,7 +1,15 @@
 package xavier.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <strong>JavaDoc</strong>
+ */
 public class MinhaClasse {
     public static void main(String[] args) {
+        // Isso é um comentário de linha
+        /*
+            Isso é um comentário de múltiplas linhas
+        */
+
         System.out.println("Olá, mundo!");
     }
 }
