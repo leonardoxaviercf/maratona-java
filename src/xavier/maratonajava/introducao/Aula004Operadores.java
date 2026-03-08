dc648c99754e411b5b626ac9995116996fd5379e
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-08 16:45:15 -0300

    Operadores relacionais

--- src/xavier/maratonajava/introducao/Aula004Operadores.java
+++ src/xavier/maratonajava/introducao/Aula004Operadores.java
@@ -2,6 +2,7 @@ package xavier.maratonajava.introducao;
 
 public class Aula004Operadores {
     public static void main(String[] args) {
+        // + - * /
         int numero01 = 10;
         int numero02 = 20;
         int resultado = numero01 + numero02;
@@ -9,5 +10,19 @@ public class Aula004Operadores {
         System.out.println("Valor: "+ resultado);
         // Para essa divisão funcionar da maneira correta, pelo menos o n1 ou n2 deverão ser double ou float
         System.out.println("Valor divisao: "+ divisao);
+
+        // %
+        int resto = numero01 % numero02;
+        System.out.println("Resto: "+ resto);
+
+        //  < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezDiferenteVinte = 10 != 20;
+        System.out.println("isDezMaiorQueVinte: "+isDezMaiorQueVinte);
+        System.out.println("isDezmenorQueVinte: "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte: "+isDezIgualVinte);
+        System.out.println("IsDezDiferenteVinte: "+isDezDiferenteVinte);
     }
 }
