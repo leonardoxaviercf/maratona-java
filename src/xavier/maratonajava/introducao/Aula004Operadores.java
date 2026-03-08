2c5aa6bb98d1c06dc78fd6f0d5d9eba082047d99
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-08 17:06:51 -0300

    Operadores lógicos

--- src/xavier/maratonajava/introducao/Aula004Operadores.java
+++ src/xavier/maratonajava/introducao/Aula004Operadores.java
@@ -24,5 +24,22 @@ public class Aula004Operadores {
         System.out.println("isDezmenorQueVinte: "+isDezMenorQueVinte);
         System.out.println("isDezIgualVinte: "+isDezIgualVinte);
         System.out.println("IsDezDiferenteVinte: "+isDezDiferenteVinte);
+
+        // && (AND) || (OR) ! (NOT)
+        int idade = 29;
+        float salario = 3500F;
+
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta: "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta: "+isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 3000;
+        float valorPlayStation = 2500F;
+
+        boolean isPlayStation5Compravel = valorTotalContaCorrente >= valorPlayStation || valorTotalContaPoupanca >= valorPlayStation;
+        System.out.println("isPlayStation5Compravel: "+isPlayStation5Compravel);
     }
 }
