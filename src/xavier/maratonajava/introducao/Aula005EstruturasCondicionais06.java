ca07e578f9fdaee2f5ff1a79148b75bda63460f7
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-09 16:41:30 -0300

    Exercício switch case

---
+++ src/xavier/maratonajava/introducao/Aula005EstruturasCondicionais06.java
@@ -0,0 +1,27 @@
+package xavier.maratonajava.introducao;
+
+public class Aula005EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        // Dado os valores de 1 a 7, imprima se é dia útil ou final de semana utilizando switch
+        // Considerando 1 como domingo
+
+        byte dia = 7;
+
+        switch (dia) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            default:
+                System.out.println("Dia inválido");
+                break;
+        }
+    }
+}
