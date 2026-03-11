0560884023b51af099250c00bce88d1aabf2026a
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-11 16:30:55 -0300

    Iniciando Arrays

---
+++ src/xavier/maratonajava/introducao/Aula007Arrays01.java
@@ -0,0 +1,14 @@
+package xavier.maratonajava.introducao;
+
+public class Aula007Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+         idades[0] = 15;
+         idades[1] = 18;
+         idades[2] = 21;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
