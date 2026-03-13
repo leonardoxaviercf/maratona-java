a9f6b68e5b324acf8a3b91512bbb389db639dc82
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-13 18:37:00 -0300

    POO - Exercício simples

---
+++ src/xavier/maratonajava/javacore/Aintroducaoclasses/domain/Carro.java
@@ -0,0 +1,7 @@
+package xavier.maratonajava.javacore.Aintroducaoclasses.domain;
+
+public class Carro {
+    public String nome;
+    public String modelo;
+    public int ano;
+}
---
+++ src/xavier/maratonajava/javacore/Aintroducaoclasses/test/CarroTest.java
@@ -0,0 +1,21 @@
+package xavier.maratonajava.javacore.Aintroducaoclasses.test;
+
+import xavier.maratonajava.javacore.Aintroducaoclasses.domain.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Palio";
+        carro1.modelo = "Básico";
+        carro1.ano = 2006;
+
+        carro2.nome = "Gol";
+        carro2.modelo = "Completo";
+        carro2.ano = 2020;
+
+        System.out.println("Nome: " + carro1.nome + " | Modelo: " + carro1.modelo + " | Ano: " + carro1.ano);
+        System.out.println("Nome: " + carro2.nome + " | Modelo: " + carro2.modelo + " | Ano: " + carro2.ano);
+    }
+}
