b10686caed72367df02e878b5ebdba9f3fdbe3ce
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-13 15:03:01 -0300

    POO - Introdução a classes

---
+++ src/xavier/maratonajava/javacore/Aintroducaoclasses/domain/Estudante.java
@@ -0,0 +1,9 @@
+package xavier.maratonajava.javacore.Aintroducaoclasses.domain;
+
+public class Estudante {
+    public String nome;
+    // Posso iniciar a variável com um valor padrao se precisar: String nome = 'Leonardo';
+    // Porém tem que ter cuidado ao usar essa opção, pois agora todos os objetos criados terão o nome = 'Leonardo'
+    public int idade;
+    public char sexo;
+}
---
+++ src/xavier/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package xavier.maratonajava.javacore.Aintroducaoclasses.test;
+
+import xavier.maratonajava.javacore.Aintroducaoclasses.domain.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Leonardo";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
