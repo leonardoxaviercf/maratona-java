17832a5129637eb87420d65e70565ef53d8b9df4
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-14 22:48:20 -0300

    POO - Returns

--- src/xavier/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
+++ src/xavier/maratonajava/javacore/Bintroducaometodos/domain/Calculadora.java
@@ -13,4 +13,28 @@ public class Calculadora {
     public void multiplicaDoisNumeros(double num1, double num2){
         System.out.println(num1 * num2);
     }
+
+    public double divideDoisNumeros(double num1, double num2){
+        if (num2 == 0) {
+            System.out.println("Impossível dividir por 0.");
+            return 0;
+        }
+
+        return num1 / num2;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2){
+        if (num2 == 0) {
+            System.out.println("Impossível dividir por 0.");
+            return;
+        }
+
+        System.out.println(num1 / num2);
+
+//      Posso fazer isso para não executar o código se num2 = 0, ou adicionar "return;" dentro do if e em seguida
+//      fazer um sout da divisão normalmente. O "return;" seria meio que o break de um metodo void
+//            else {
+//            System.out.println(num1 / num2);
+//        }
+    }
 }
---
+++ src/xavier/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,16 @@
+package xavier.maratonajava.javacore.Bintroducaometodos.test;
+
+import xavier.maratonajava.javacore.Bintroducaometodos.domain.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        double result = calculadora.divideDoisNumeros(100, 0);
+        System.out.println(result);
+
+        System.out.println("------------");
+
+        calculadora.imprimeDivisaoDeDoisNumeros(20, 0);
+    }
+}
