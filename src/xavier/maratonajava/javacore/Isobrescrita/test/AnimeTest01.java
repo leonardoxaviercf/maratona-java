be9288f7884a9ed9ce659c8d3fa7c59c92662604
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-19 17:42:28 -0300

    POO - Sobrescrita do método toString

---
+++ src/xavier/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package xavier.maratonajava.javacore.Isobrescrita.test;
+
+import xavier.maratonajava.javacore.Isobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+
+        System.out.println(anime);
+    }
+}
