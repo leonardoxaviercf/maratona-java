c41f85801c148d5fa2541ef4b551c3fd07c01967
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-25 15:24:08 -0300

    Excções - RuntimeExcption

--- src/xavier/maratonajava/javacore/Oexecption/error/test/StackOverflowTest01.java
+++ src/xavier/maratonajava/javacore/Oexecption/error/test/StackOverflowTest01.java
@@ -1,4 +1,4 @@
-package xavier.maratonajava.javacore.Oexecption.test;
+package xavier.maratonajava.javacore.Oexecption.error.test;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
