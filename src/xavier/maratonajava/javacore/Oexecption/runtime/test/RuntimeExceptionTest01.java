0b62dc47e7902c8263472ac4d491e543b1940665
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-25 21:08:14 -0300

    Exceções - RuntimeException

--- src/xavier/maratonajava/javacore/Oexecption/runtime/test/RuntimeExceptionTest01.java
+++ src/xavier/maratonajava/javacore/Oexecption/runtime/test/RuntimeExceptionTest01.java
@@ -3,10 +3,7 @@ package xavier.maratonajava.javacore.Oexecption.runtime.test;
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
         // CHECKED e UNCHECKED
-        Object object = null;
-
-//        System.out.println(object.toString());
-        int[] num = {1, 2};
-        System.out.println(num[2]);
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
     }
 }
