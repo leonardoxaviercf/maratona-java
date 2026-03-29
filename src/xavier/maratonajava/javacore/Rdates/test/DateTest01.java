f4baf6680341f4c817c6a6ccd1b10b1938233035
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-28 21:35:46 -0300

    Date

---
+++ src/xavier/maratonajava/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,10 @@
+package xavier.maratonajava.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1712341422432L);
+        System.out.println(date);
+    }
+}
