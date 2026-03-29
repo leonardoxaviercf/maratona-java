f96d09fd6c911ccac27adb519c66d200e72067a3
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-29 09:08:45 -0300

    Internacionalização de moedas

---
+++ src/xavier/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -0,0 +1,25 @@
+package xavier.maratonajava.javacore.Sformatacao.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest02 {
+    public static void main(String[] args) {
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[4];
+
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+
+        double valor = 10000.2130;
+
+        for (NumberFormat nf : nfa) {
+            System.out.println(nf.getMaximumFractionDigits());
+            System.out.println(nf.format(valor));
+        }
+    }
+}
