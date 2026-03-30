4b8c17c875bd3a440c45ba5393bf34c69e1ffede
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-03-30 15:02:28 -0300

    Instant

---
+++ src/xavier/maratonajava/javacore/Rdates/test/InstantTest01.java
@@ -0,0 +1,18 @@
+package xavier.maratonajava.javacore.Rdates.test;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+        System.out.println(now.getEpochSecond());
+        System.out.println(now.getNano());
+        System.out.println(Instant.ofEpochSecond(3, 1000000));
+        System.out.println(Instant.ofEpochSecond(3, 1000000000));
+        System.out.println(Instant.ofEpochSecond(3, -1000000000));
+    }
+}
