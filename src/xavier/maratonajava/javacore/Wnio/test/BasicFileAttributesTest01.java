1197e46505d7760d90032af30d96e311ebe044c9
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-04-03 16:02:19 -0300

    NIO - BasicFileAttributes

---
+++ src/xavier/maratonajava/javacore/Wnio/test/BasicFileAttributesTest01.java
@@ -0,0 +1,36 @@
+package xavier.maratonajava.javacore.Wnio.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class BasicFileAttributesTest01 {
+    public static void main(String[] args) throws IOException {
+        // BasicFileAttributes, DosFileAttributes, PosixFileAttributes
+        LocalDateTime date = LocalDateTime.now().minusDays(10);
+        File file = new File("pasta2/novo.txt");
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        boolean isModified = file.setLastModified(date.toInstant(ZoneOffset.UTC).toEpochMilli());
+
+        Path path = Paths.get("pasta2/novo_path.txt");
+//        Files.createFile(path);
+
+        FileTime ft = FileTime.from(date.toInstant(ZoneOffset.UTC));
+
+        Files.setLastModifiedTime(path, ft);
+        System.out.println(Files.isWritable(path));
+        System.out.println(Files.isReadable(path));
+        System.out.println(Files.isExecutable(path));
+    }
+}
