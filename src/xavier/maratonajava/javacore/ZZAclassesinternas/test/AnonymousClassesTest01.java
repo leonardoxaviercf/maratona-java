717c874d484d238e539803015d086b9a02fe84ae
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-04-21 14:33:52 -0300

    Classes anômimas

---
+++ src/xavier/maratonajava/javacore/ZZAclassesinternas/test/AnonymousClassesTest01.java
@@ -0,0 +1,19 @@
+package xavier.maratonajava.javacore.ZZAclassesinternas.test;
+
+class Animal{
+    public void walk(){
+        System.out.println("Animal walking");
+    }
+}
+
+public class AnonymousClassesTest01 {
+    public static void main(String[] args) {
+        Animal animal = new Animal(){
+            @Override
+            public void walk(){
+                System.out.println("Dog walking in the shadows");
+            }
+        };
+        animal.walk();
+    }
+}
