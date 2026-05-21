149bff2c850889e6d2497be71bff9721c6368294
Leonardo Xavier <leonardocruzfilho8@gmail.com>
2026-05-21 14:20:13 -0300

    Thread - Lock e ReetrantLock

--- src/xavier/maratonajava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
+++ src/xavier/maratonajava/javacore/ZZGconcorrencia/test/AtomicIntegerTest01.java
@@ -1,13 +1,23 @@
 package xavier.maratonajava.javacore.ZZGconcorrencia.test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Counter {
     private int count;
     private AtomicInteger atomicInteger = new AtomicInteger();
+    private Lock lock = new ReentrantLock();
     void increment() {
-        count++;
-        atomicInteger.incrementAndGet();
+        lock.lock();
+        try {
+            count++;
+            atomicInteger.incrementAndGet();
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     public int getCount() {
