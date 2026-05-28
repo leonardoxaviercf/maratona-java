26c745f1bf889c7a4ebb0c818bd2f347cafb22c3
Leonardo Xavier <leonardocruzfilho8@gmail.com>
2026-05-28 14:15:55 -0300

    Padrões de projeto - Lazy Initialization

---
+++ src/xavier/maratonajava/javacore/ZZHpadroesdeprojeto/test/AircraftSingletonLazyTest01.java
@@ -0,0 +1,16 @@
+package xavier.maratonajava.javacore.ZZHpadroesdeprojeto.test;
+
+import xavier.maratonajava.javacore.ZZHpadroesdeprojeto.domain.AircraftSingletonLazy;
+
+public class AircraftSingletonLazyTest01 {
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A");
+    }
+
+    private static void bookSeat(String seat) {
+        System.out.println(AircraftSingletonLazy.getINSTANCE());
+        AircraftSingletonLazy aircraft = AircraftSingletonLazy.getINSTANCE();
+        System.out.println(String.valueOf(aircraft.bookSeat(seat)));
+    }
+}
